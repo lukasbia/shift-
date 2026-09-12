d8e642a28fdef8fc51b54627245d429192c41b77
lukasbia <stodomingokylijahnver@gmail.com>
2026-09-12 10:56:32 +0800

    Update SourceLocation.swift

--- ShiftCompilerSource/Source/Basic/Lexer/SourceLocation.swift
+++ ShiftCompilerSource/Source/Basic/Lexer/SourceLocation.swift
@@ -1,18 +1,24 @@
-//
-// SourceLocation.swift
-// Shift
-//
+public struct SourceLocation: Equatable, Hashable, Comparable, Sendable {
+    public let line: Int
+    public let column: Int
 
-struct SourceLocation: Equatable, Hashable {
-
-    let line: Int
-    let column: Int
-
-    init(
-        line: Int,
-        column: Int
-    ) {
+    public init(line: Int, column: Int) {
         self.line = line
         self.column = column
+    }
+
+    public static func < (
+        lhs: SourceLocation,
+        rhs: SourceLocation
+    ) -> Bool {
+        if lhs.line != rhs.line {
+            return lhs.line < rhs.line
+        }
+
+        return lhs.column < rhs.column
+    }
+
+    public var description: String {
+        "\(line):\(column)"
     }
 }
